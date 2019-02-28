4cf6eecdf5aeea5305c0c759d71255ab69059b7f
notaturkey <tommytocool5@gmail.com>
2019-02-28 19:52:33 +0000

    asdf

--- asl/settings.py
+++ asl/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
+import django
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -121,3 +122,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
+django.setup()
