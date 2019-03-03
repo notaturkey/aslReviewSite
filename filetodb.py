157142dbe93fbe75deb221cdbd2c8925dbdfe4a4
notaturkey <tommytocool5@gmail.com>
2019-03-03 22:07:29 +0000

    fix

--- filetodb.py
+++ filetodb.py
@@ -2,7 +2,7 @@ from polls.models import word
 
 with open('./aslvocab1.txt') as f:
     for line in f:
-        q = word(random_word = line, url = 'https://student.truewayasl.com/?s=%' + line)
+        q = word(random_word = line, url = 'https://student.truewayasl.com/?s=' + line)
         q.save()
         
 from unit2.models import word2
--- polls/views.py
+++ polls/views.py
@@ -9,15 +9,15 @@ from .models import word
 def index(request):
     template = loader.get_template('polls/index.html')
     latest_word_list = word.objects.all()
-    context = {'latest_word_list': latest_word_list, 'rando_num': random.randint(1,117)}
+    context = {'latest_word_list': latest_word_list, 'rando_num': random.randint(1,332)}
     return HttpResponse(template.render(context, request))
     
 def detail(request, random_wor):
     template = loader.get_template('polls/detail.html')
     wordet = word.objects.get(pk = random_wor) 
     latest_word_list = word.objects.all()
-    prev= 117 if(random_wor - 1 < 1) else random_wor - 1
-    next= 1 if(random_wor + 1 > 117) else random_wor + 1
-    context = {'latest_word_list': latest_word_list,'random_word': wordet.random_word,'rando_num': random.randint(1,117), 'url': wordet.url.replace(" ", "+"), id: random_wor,'prev': prev, 'nxt':next}  
+    prev= 332 if(random_wor - 1 < 1) else random_wor - 1
+    next= 1 if(random_wor + 1 > 332) else random_wor + 1
+    context = {'latest_word_list': latest_word_list,'random_word': wordet.random_word,'rando_num': random.randint(1,332), 'url': wordet.url.replace(" ", "+"), id: random_wor,'prev': prev, 'nxt':next}  
     return HttpResponse(template.render(context, request))
     
